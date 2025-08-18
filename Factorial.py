dfc6543135bc90987acf2859719b75369c69e29b
Turjo <turjosarma@gmail.com>
2025-08-18 08:05:09 -0700

    New feature added

--- Factorial.py
+++ Factorial.py
@@ -3,4 +3,8 @@ a = int(input("Enter the number :"))
 fact = 1
 for i in range(1,(a + 1)):
     fact *= i
-    print(fact)
+print(f"The factorial of the number {a} is {fact}")
+
+print("Square mode")
+b = int(input("Enter the number :"))
+print(f"The square of number {b} is {b**2}")
